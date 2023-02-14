f73c97bcbcee8d05c4de57420cd287f6818c43c5
jaehyunlee <jaehyunlee25@gmail.com>
2023-02-14 16:37:51 +0900

    'mqtt_golf_renew'

--- ws.js
+++ ws.js
@@ -221,10 +221,6 @@ function sendMessage(json, jsonMsg) {
   });
 }
 function setLogReport(macroId, deviceId, clubId, device, type, result, others) {
-  if (result == "normal") {
-    console.log("normal pass", deviceId, clubId, device, type, result);
-    return;
-  }
   const param = { macroId, deviceId, clubId, device, type, result, others };
   "sql/setLogReport.sql".gfdp(param).query((err, result, fields) => {
     if (err) {
